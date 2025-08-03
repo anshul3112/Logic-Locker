3f260208a972e2e4b8d4d83f574b47a60248e493
anshul3112 <singhanshul3112@gmail.com>
2025-08-03 17:37:22 +0530

    changed fetch calls

--- Backend/app.js
+++ Backend/app.js
@@ -10,13 +10,7 @@ const app = express();
 const allowedOrigins = process.env.CORS_ORIGIN.split(",");
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: [process.env.CORS_ORIGIN, "http://localhost:5173"], // or 3000 based on Vite config
   credentials: true,
 }));
 
