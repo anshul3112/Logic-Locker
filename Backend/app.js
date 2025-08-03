550b4b5335b5873b5547a844d826eaf90e263c8b
anshul3112 <singhanshul3112@gmail.com>
2025-08-03 16:51:12 +0530

    cors added in backend

--- Backend/app.js
+++ Backend/app.js
@@ -7,9 +7,18 @@ import summaryRouter from './routes/summary.route.js';
 
 const app = express(); 
 
+const allowedOrigins = process.env.CORS_ORIGIN.split(",");
+
 app.use(cors({
-    origin : process.env.CORS_ORIGIN,
-}))
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+}));
 
 app.use(express.json({
     limit : "16kb"
